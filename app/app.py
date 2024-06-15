af37ff0c0c0dcd1f019a5f69fca832ea62acf98f
Devam <satasiyadevam@gmail.com>
2024-06-15 22:27:50 +0530

    initial commit

---
+++ app/app.py
@@ -0,0 +1,10 @@
+# app.py
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Addition of 2 and 3:", add(2, 3))
+    print("Subtraction of 5 and 2:", subtract(5, 2))
